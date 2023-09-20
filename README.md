04e1d81ac8c1ed603a8533207790eda81f0a2e7c
moremeyou <moremeyou@users.noreply.github.com>
2023-09-20 11:24:41 +0200

    More functions
    See the readme for updates.

--- README.md
+++ README.md
@@ -1,23 +1,41 @@
-# Tag Buddy
+# Obsidian Tag Buddy
 
 A collection of tag functionalities for Obsidian. 
 
-**Functionality 1:** Quick delete in-line 
+**Use 1:** Quick delete tag in-line
 
-Replicates the quick delete functionality of the #tagName(x) in YAML or properties panel. Why? I use tags to connect ideas, but also as a flexible I/O or state/status management system. For example, most of my content comes in through daily notes with tags. Then I have specialized notes that query those tags into an "Inbox" section of the specialized note so I can review on-demand. This functionality saves a few clicks, and with future updates, a few more. 
+Replicates the quick delete functionality of the #tagName(x) in YAML or properties panel. 
+
+**Use 2:** Remove child tags in-line
+
+If a tag has children, Use 1 will progressively remove them before removing the tag completely.
+
+**Use 3:** Convert tag to text
+
+OPT/ALT+CLICK removes the hash from the tag text.  
+
+**How I use**
+
+Why is this useful to me? I use tags to connect ideas, but also as a flexible I/O or state/status management system. For example, most of my content comes in through daily notes with tags. Then I have specialized notes that query those tags into an "Inbox" section of the specialized note/tag so I can review on-demand. In this workflow, tapping or removing a tag saves a few clicks, and with future updates, a few more. 
 
 **How to use**
 - Click/tap a tag in reading mode and it delete from your note.
 - Hold ctrl/cmd+click to preserve the native functionality (tag search).
-- For now, only use this with tags in the current note. 
 - For now, the functionality only works in reading mode. 
-- Switch to editing to undo any deletions. 
+- Switch to editing to undo any deletions. Deletions are permanent when applied to unopened files.
 - Works best with [Tag Summary](https://github.com/macrojd/tag-summary) and [Colored Tags](https://github.com/pfrankov/obsidian-colored-tags) plugins. 
+- ALWAYS BACKUP YOUR VAULT BEFORE USING NEW PLUGINS. 
 
 **To Do:**
-- [ ] Optimizations 
+- [*] Optimizations 
+- [ ] More optimizations
+- [*] Settings to customize removal and convert (alt+click removes #, cmd+click native search)
 - [ ] Bug: mobile tapping deletes tag AND opens tag search
+- [ ] Bug: should be ignoring the yaml and code blocks. indexs will need to account for this
+- [*] Dynamic editing: Delete tags from Tag Summary (requires my modifed Tag Summary plugin)
+- [ ] Dynamic editing: Integrate Tag Summary and/or find another way to know if this is "summary"
+- [ ] Dynamic editing support for embeds
 - [ ] Rollover turns the tag background red
-- [ ] Delete tags from embedded notes. 
-- [ ] Delete tags from queried embedded notes (via Tag Summary). 
-- [ ] More functionalites.
+- [ ] Ability to progressively remove child tags.
+- [ ] Test functionality in editing/live-preview mode
+- [ ] More Tag Buddies (functionalites). Suggestions?
--- main.ts
+++ main.ts
@@ -1,142 +1,194 @@
+import { TBSettingsTab } from "./settings";
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-export default class ClickAnywherePlugin extends Plugin {
-  onload() {
-    //console.log('ClickAnywherePlugin loaded successfully.');
-    
-    //this.collectTagIds(); 
-    
-    // Re-collect tag ids on various events
-    this.registerEvent(this.app.on('editor-change', (event: EditorEvent) => {
-      this.collectTagIds(); 
-    }));
-    this.registerEvent(this.app.on('file-open', (event: EditorEvent) => {
-      this.collectTagIds(); 
-    }));
-    this.registerEvent(this.app.on('layout-change', (event: EditorEvent) => {
-      this.collectTagIds(); 
-    })); 
+// Bug: should be ignoring the yaml and any code blocks. indexs will need to account for this
 
-    // Main logic for removing clicked tag
-    this.registerDomEvent(document, 'mousedown', (event: MouseEvent) => { 
-      this.collectTagIds(); 
-      if (event.metaKey || event.ctrlKey) { 
-        return;
-      }
-      const clickedTagElement = this.getClickedTag(event.target);
-      //console.log(clickedTagElement);
-      if (clickedTagElement) {
-        const clickedTagText = clickedTagElement.textContent.trim();
-        const fullTagText = clickedTagText;
-        const tagId = parseInt(clickedTagElement.getAttribute('data-tag-id'));
+interface TBSettings {
+	removeOnClick: boolean; //ctrl
+	optToConvert: boolean; //alt
+}
+
+const DEFAULT_SETTINGS: Partial<TBSettings> = {
+	removeOnClick: true, // when true, cmd is needed when clicking to remove the tag
+	optToConvert: true, // when false, clicking tag will do nothing
+}; 
 
 
-        // Determine if the clicked tag is in the active note
-        const activeNoteContainer = this.app.workspace.activeLeaf.containerEl;
-        const isTagInActiveNote = isElementInActiveNote(clickedTagElement, activeNoteContainer);
-        //console.log('Is tag in active note:', isTagInActiveNote);
-        // Now you can use isTagInActiveNote to decide whether to remove the tag or not
+export default class TagBuddy extends Plugin {  
+	settings: TBSettings;
+
+	onunload() {}
   
+	async onload() {
+		// This adds a settings tab so the user can configure various aspects of the plugin 
+		await this.loadSettings();
+		this.addSettingTab(new TBSettingsTab(this.app, this));
+
+		console.log('Tag Tests Plugin loaded!');
+
+		this.registerDomEvent(document, 'click', async (event: MouseEvent) => {
+		const target = event.target as HTMLElement;
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		//console.log('removeOnClick: ' + this.settings.removeOnClick);
+		//console.log('optToConvert: ' + this.settings.optToConvert);
+		//console.log('event.metaKey = ' + event.metaKey);
+		//console.log('(event.ctrlKey && !this.settings.removeOnClick) = ' + (event.ctrlKey && !this.settings.removeOnClick));
+		//console.log('(event.metaKey && !this.settings.removeOnClick) = ' + (event.metaKey && !this.settings.removeOnClick));
+		if (this.settings.removeOnClick && event.metaKey) {
+			return;
+		} else if (!this.app.workspace.getActiveViewOfType(MarkdownView) == 'preview'){ 
+			return;
+		} else if (event.altKey && !this.settings.optToConvert) {
+			return;
+		}
+		
+		if (target && target.matches('.tag')) {
+			//console.log('LET\'S DO THIS!!!');
+			
+			if (this.settings.removeOnClick) {
+				event.preventDefault();
+				event.stopPropagation();
+			}
+
+			const tag = target.innerText;
+			//console.log(`Clicked tag: ${tag}`);
+			let element = target as HTMLElement;
+			const activeNoteContainer = this.app.workspace.activeLeaf.containerEl;
+			const isTagInActiveNote = isElementInActiveNote(target, activeNoteContainer);
+
+			let file = this.app.workspace.getActiveFile();
+			if (!isTagInActiveNote) {
+				while (element) {
+					const linkElement = element.querySelector('a[data-href$=".md"]');
+					if (linkElement) {
+						const filePath = linkElement.getAttribute('data-href');
+						file = this.app.vault.getAbstractFileByPath(filePath);
+						break;
+					}
+					element = element.parentNode;
+				}
+			}
+			
+			//view.previewMode.rerender(true);
+			//this.app.workspace.activeLeaf.rebuildView()
+			//this.app.workspace.getActiveLeaf().refresh();
+			const activeLeaf = this.app.workspace.activeLeaf;
+			if (activeLeaf && !isTagInActiveNote) {
+			    // ... other operations
+		    	activeLeaf.rebuildView();
+			}
 
 
-        this.getTagLocation(fullTagText, tagId).then((tagLocation) => {
-          if (tagLocation && isTagInActiveNote) {
-            //console.log('remove tag');
-            this.removeTagTextFromNote(fullTagText, tagLocation, tagId);  
-            this.collectTagIds(); 
-          }
-        }); 
-      } 
-    });
-  
+			//console.log(`In current file? ${isTagInActiveNote?'yes':'no'}`);
 
-  function isElementInActiveNote(clickedElement, activeNoteContainer) {
-  let element = clickedElement;
-  while (element) {
-    if (element === activeNoteContainer) {
-      return true;
-    }
-    if (element.classList.contains('summary')) {
-      //console.log('child element: ' + element.classList);
-      return false;
-    }
-    element = element.parentElement;
-  }
-  return false;
+			if (file) {
+				const fileContent = await this.app.vault.read(file);
+				// Count occurrences of the tag
+				const tags = Array.from((isTagInActiveNote?document:element).querySelectorAll('.tag'));
+				const occurrenceIndex = tags.filter(t => t.innerText === tag && t.offsetTop < target.offsetTop).length;
+				const occurrences = fileContent.split(tag).length - 1;
+				let startIndex = -1;
+				for (let i = 0; i <= occurrenceIndex; i++) {
+					startIndex = fileContent.indexOf(tag, startIndex + 1); 
+				}
+
+				/*console.log(`Occurrence index: ${occurrenceIndex}`);
+				console.log(`File: ${file.basename}`);
+				console.log(`Total occurrences: ${occurrences}`); 
+				console.log(`Starting index: ${startIndex}`);*/
+
+				//console.log(event.metaKey + ' | ' + event.altKey);
+				// convert tag to an in-line tag summary below this line? or create a line break?
+
+				if (startIndex !== -1) {
+					const contentBeforeTag = fileContent.substring(0, startIndex);
+					const contentAfterTag = fileContent.substring(startIndex);
+					let newContent = '';
+					//const newContent = contentBeforeTag + 'WORKED ' + contentAfterTag;
+					if (event.altKey) {
+						//console.log('altkeyu');
+						//newContent = fileContent.substring(0, startIndex+1) + fileContent.substring(startIndex + tag.length);
+						newContent = fileContent.substring(0, startIndex) + tag.substring(1) + fileContent.substring(startIndex + tag.length);
+					} else if ((event.metaKey && !this.settings.removeOnClick) || (!event.metaKey && this.settings.removeOnClick)) {
+						//console.log(event.ctrlKey + ' | ' + this.settings.removeOnClick);
+						//console.log(!event.ctrlKey && !this.settings.removeOnClick);
+						//console.log(event.ctrlKey && this.settings.removeOnClick);
+						//newContent = fileContent.substring(0, startIndex) + fileContent.substring(startIndex + tag.length);
+						newContent = fileContent.substring(0, startIndex - (startIndex>0 && fileContent[startIndex-2]===' '?2:1)) + fileContent.substring(startIndex + tag.length);
+					} else {
+						return;
+					}
+					
+					await this.app.vault.modify(file, newContent);
+
+					this.app.workspace.iterateLeaves(leaf => {
+						const markdownView = leaf.view as MarkdownView;
+						if (markdownView.file && markdownView.file.path === file.path) {
+							const editor = markdownView.sourceMode.cmEditor;
+							editor.setValue(newContent);
+						}
+					});
+				}
+			}
+				
+			if (!isTagInActiveNote) { 
+				setTimeout(async () => {
+					//app.workspace.activeLeaf.rebuildView();
+					//app.workspace.getActiveViewOfType(MarkdownView)?.previewMode.rerender(true);
+					//view.previewMode.rerender(true);
+					app.workspace.activeLeaf.rebuildView()
+				}, 1)};
+			}
+		}, true); // (this.settings.removeOnClick || this.settings.optToRemove)?true:false);
+
+		function isElementInActiveNote(clickedElement, activeNoteContainer) {
+			let element = clickedElement;
+			while (element) {
+				if (element === activeNoteContainer) {
+					return true;
+				}
+				if (element.classList.contains('summary')) {
+					//console.log('child element: ' + element.classList);
+					return false;
+				}
+				element = element.parentElement;
+			}
+			return false;
+		}
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
 }
 
-}
 
+/*class TBSettingTab extends PluginSettingTab {
+	plugin: TagBuddy;
 
-  // Collect all tags and set data-tag-id attribute
-  async collectTagIds() {
-    const note = this.app.workspace.getActiveFile();  
+	constructor(app: App, plugin: TagBuddy) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
 
-    if (note) {
-      //console.log('collectTagIds');
-      const content = await app.vault.read(note);
+	display(): void {
+		const {containerEl} = this;
 
-      const contentElement = this.app.workspace.activeLeaf.containerEl;
+		containerEl.empty();
 
-      const tagElements = contentElement.querySelectorAll('.tag');     
-      const tagCounts = {};
-
-      tagElements.forEach((tagElement) => {
-      const tagText = tagElement.textContent.trim();
-      const fullTagText = '#' + tagText;
-
-      if (!tagCounts.hasOwnProperty(fullTagText)) { 
-        tagCounts[fullTagText] = 0;
-      }
-
-      tagElement.setAttribute('data-tag-id', tagCounts[fullTagText]);
-      tagCounts[fullTagText]++;
-    });
-    
-  }
-}
-
-  // Return target if it's a tag, otherwise return null
-  getClickedTag(target) {
-    if (target.classList && target.classList.contains('tag')) {
-      return target;
-    }
-    return null;
-  }
-
-  // Get start and end index of the tag to be removed
-  async getTagLocation(fullTagText, tagId) {
-    const note = this.app.workspace.getActiveFile();
-
-    if (note) {
-      const content = await this.app.vault.read(note);
-      let startIndex = -1;
-
-      for (let i = 0; i <= tagId; i++) {
-        startIndex = content.indexOf(fullTagText, startIndex + 1); 
-      }
-
-      if (startIndex !== -1) {
-        const endIndex = startIndex + fullTagText.length;
-        return { startIndex, endIndex };
-      }
-    }
-
-    return null; 
-  }
-
-  // Remove tag from note and update content
-  async removeTagTextFromNote(fullTagText, tagLocation) {
-    const note = this.app.workspace.getActiveFile();
-
-    if (note) {
-      const content = await this.app.vault.read(note);
-
-      const newContent = content.substring(0, tagLocation.startIndex) + content.substring(tagLocation.endIndex);
-
-      await this.app.vault.modify(note, newContent);
-    }
-    //this.collectTagIds(); 
-  } 
-
-}
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}*/
